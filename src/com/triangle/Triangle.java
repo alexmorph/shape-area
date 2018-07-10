74a42d89c8d91d393700c60eb63108279efd748e
Alexandr Miliukov <7466966@gmail.com>
2018-07-10 18:36:09 +0300

    edit package

---
+++ src/com/triangle/Triangle.java
@@ -0,0 +1,27 @@
+package com.triangle;
+
+import figures.AbstractShape;
+import figures.Shape;
+
+public class Triangle extends AbstractShape implements Shape {
+	
+	private int side_A, side_B, side_C;
+	
+	public Triangle(int sideA, int sideB, int sideC) {
+		this.side_A = sideA;
+		this.side_B = sideB;
+		this.side_C = sideC;
+	}
+
+	@Override
+	public double getArea() {
+		double semiperimetr = (side_A + side_B + side_C) / 2.0;  
+		 double result = Math.sqrt(semiperimetr * (semiperimetr - side_A) * 
+				 (semiperimetr - side_B) * (semiperimetr - side_C)); 
+		return result;
+	}
+	
+
+
+
+}
