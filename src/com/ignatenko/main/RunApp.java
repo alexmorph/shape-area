d6f6e1d56ab3d53e569e4ec7def25ae4fec82dd4
Alexandr Miliukov <7466966@gmail.com>
2018-07-18 22:27:56 +0300

    edit runApp

--- src/com/ignatenko/main/RunApp.java
+++ src/com/ignatenko/main/RunApp.java
@@ -20,20 +20,8 @@ import com.ignatenko.triangle.Triangle;
 
 public class RunApp {
 	private static final Logger logger = Logger.getLogger(RunApp.class.getName());
-	private static FindValue findValue;
 
 	public static void main(String[] args) {
-
-		// Circle cirlce = new Circle();
-		// cirlce.setRadius(5);
-		// logger.log(Level.INFO, String.valueOf(cirlce.getArea()));
-
-		// Square square = new Square();
-		// square.setSideOfSquare(0);
-		// logger.log(Level.INFO, String.valueOf(square.getArea()));
-
-		Triangle triangle = new Triangle(2, 4);
-		// logger.log(Level.INFO, String.valueOf(triangle.getArea()));
 
 		List<Shape> figures = new ArrayList<>();
 		figures.add(new Circle(4));
@@ -46,37 +34,25 @@ public class RunApp {
 		figures.add(new Triangle(5, 9));
 		figures.add(new Triangle(6, 10));
 
-		// Map<String, Integer> ñircleValues = new HashMap<>();
-		// List<Integer> SquareValues = new ArrayList<>();
-		// List<Integer> TriangleValues = new ArrayList<>();
+		Shape minimalFigure = null;
+		Shape maximalFigure = null;
 
-		Class<? extends Shape> minimalFigure = null;
-		Class<? extends Shape> maximalFigure = null;
+if (!figures.isEmpty()) {
+	minimalFigure = figures.get(0);
+	maximalFigure = figures.get(0);
+}
 
-		int max = Integer.MIN_VALUE;
-		int min = Integer.MAX_VALUE;
+for (int i = 0; i < figures.size(); i++) {
+	if( maximalFigure.getArea() < figures.get(i).getArea())
+		maximalFigure = figures.get(i);
+	if( i >= 1 && minimalFigure.getArea() > figures.get(i).getArea())
+		minimalFigure = figures.get(i);
+}
 
-		for (Shape i : figures) {
-			if (i.getArea() > max) {
-				max = i.getArea();
-				maximalFigure = i.getClass();
-			}
-			if (i.getArea() < min) {
-				min = i.getArea();
-				minimalFigure = i.getClass();
-			}
-		}
+		logger.log(Level.INFO, "Min area contain in class: " +minimalFigure.getClass().getSimpleName() + " area: " + minimalFigure.getArea());
+		logger.log(Level.INFO, "Max area contain in class: " +maximalFigure.getClass().getSimpleName() + " area: " + maximalFigure.getArea());
 
-		logger.log(Level.INFO, "Min area contain in class: " +minimalFigure.getName() + " area: " + min);
-		logger.log(Level.INFO, "Max area contain in class: " +maximalFigure.getName() + " area: " + max);
 
-		// for(Map.Entry<String, Integer> entry : ñircleValues.entrySet() ) {
-		// System.out.println(entry.getKey() + " " +entry.getValue());
-		// }
-
-		// System.out.println(Collections.max(CircleValues.values()) + " "
-		// +CircleValues.getClass());
-		// System.out.println(Collections.min(CircleValues));
 
 	}
 
